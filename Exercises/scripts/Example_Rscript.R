0b5d06223de488a37d16446c27e7a3fff727245b
MartinaHYang <hong.yang2@mail.mcgill.ca>
2026-02-04 22:31:23 -0500

    update

---
+++ Exercises/scripts/Example_Rscript.R
@@ -0,0 +1,13 @@
+#This is an example R script
+#It is much simpler than an R Markdown but is very useful for automation
+#Can be edited & rendered with any text editor
+
+
+print('hello world')
+
+2*5
+
+2+2
+
+
+3^3
